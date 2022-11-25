f89360ba13cf39976b45825f29d5ac763a4e387b
Orel <orelipello@gmail.com>
2022-11-25 15:12:54 +0100

    Create application's structure

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, render_template_string
+import pandas as pd
+import numpy as np
+import pickle
+
+
+
+# Create application
+app = Flask(__name__)
+
+
+# ------------------------------------------
+# ROUTING
+#-------------------------------------------
+
+#Link to the HOMEPAGE (endpoint /)
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+#(debug = True) Refresh the page without always having to restart the exe
+if __name__ == "__main__":
+    app.run(debug = True)
