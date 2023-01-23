7c70429fb3c3b1a1b86cc16cea5ce0e61545c169
Orel <orelipello@gmail.com>
2023-01-23 17:42:30 +0100

    fix : Correction of request, show recommendations

--- app.py
+++ app.py
@@ -49,15 +49,6 @@ def executing_training_model():
 #    print("if File is found, load 'KNN_articles.pkl'") 
 #    MODEL = pkl.load(open('KNN_articles.pkl','rb'))
 
-#_, predictions, top_recommendation, MODEL, RMSE_KNN = executing_training_model()
-
-#Testing on user 458
-# user = 458
-# reco_for_user = find_recommendation(top_recommendation, user)
-# print("Reco for user", user, ":", reco_for_user)
-
-# print("testing app")
-
 
 # ------------------------------------------
 # ROUTING
@@ -95,24 +86,55 @@ def predict():
 
     df_dropped = pd.read_csv("df_dropped.csv")
 
-    # Get user_id for selector
+    # get user_id for selector
     options_id_for_dropdown = []
     for idx in enumerate(df_dropped.user_id.unique()):
         options_id_for_dropdown.append(idx[1])
         # Order list by ascending values
-        options_id_for_dropdown= sorted(options_id_for_dropdown)
+        options_id_for_dropdown = sorted(options_id_for_dropdown)
     
-    _, predictions, top_recommendation, MODEL, RMSE_KNN = executing_training_model()
+    # get article_id for adding recommendations' details in modal
+    get_article_id = []
+    for art in enumerate(df_dropped.article_id.unique()):
+        get_article_id.append(art[1])
+        # Order list by ascending values
+        get_article_id = sorted(get_article_id)
+
+    # get publisher_id for adding recommendations' details in modal
+    get_publisher_id = []
+    for pub in enumerate(df_dropped.publisher_id.unique()):
+        get_publisher_id.append(pub[1])
+        # Order list by ascending values
+        get_publisher_id = sorted(get_publisher_id)
+  
+    # get category_id for adding recommendations' details in modal
+    get_category_id = []
+    for cat in enumerate(df_dropped.category_id.unique()):
+        get_category_id.append(cat[1])
+        # Order list by ascending values
+        get_category_id = sorted(get_category_id)
+    
+    _, _, top_recommendation, _, _ = executing_training_model()
 
     if request.method == 'POST':
-        input_user = 458
-        #input_user = request.args.get("input_user") 
+        input_user = request.form.get('input_user') 
+        input_user = int(input_user)
         print("input_user", input_user)
-
         reco_for_user = find_recommendation(top_recommendation, input_user)
+        print("reco_for_user", reco_for_user)
+
+    # Create a list of the best articles' rating for inserting into a carrousel's recommendation
+    df_best_session = df_dropped[df_dropped.session_size >= 5].sort_values(by="session_size", ascending=False)
+    print("df_best_session", df_best_session.head())
+    print("df_best_session shape", df_best_session.shape)
+
+    best_session_embeddings = []
+    for x in df_best_session.itertuples():
+        best_session_embeddings.append(x.articles_embeddings)
+        #Show only the first embedding for testing
+        print(best_session_embeddings[0])
         
-    return render_template('index.html', input_user = input_user, reco_for_user = reco_for_user, df_dropped= df_dropped, options_id_for_dropdown=options_id_for_dropdown)
-    # return redirect('/')  
+    return render_template('index.html', input_user = input_user, reco_for_user = reco_for_user, df_dropped= df_dropped, options_id_for_dropdown=options_id_for_dropdown, best_session_embeddings=best_session_embeddings, get_article_id=get_article_id, get_category_id=get_category_id, get_publisher_id=get_publisher_id)
 
 
 #(debug = True) Refresh the page without always having to restart the exe
